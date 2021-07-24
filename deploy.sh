40f1f0f3013b9ae3641d8971bae02c709d354ba3
David <littletiger58@gmail.com>
2021-07-24 16:50:44 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:chindit/chindit.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chindit/dadashopping.git main:gh-pages
+#git push -f git@github.com:chindit/dadashopping.git main:gh-pages
 
 cd -
