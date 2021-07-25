0f38e650e1e614b713163cd677a8bc21f783f53b
David <littletiger58@gmail.com>
2021-07-25 10:31:02 +0200

    gh-pages

---
+++ precache-manifest.a9a14cb9f42c6c624ae1fec9d7d393f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e33f4e512775eff424e",
+    "url": "/dadashopping/css/app.3a2158fb.css"
+  },
+  {
+    "revision": "1615f9cea71a91d92d7a53d09c7c207f",
+    "url": "/dadashopping/index.html"
+  },
+  {
+    "revision": "0e33f4e512775eff424e",
+    "url": "/dadashopping/js/app.ca19f1b3.js"
+  },
+  {
+    "revision": "f5a175acc4b0fe640b66",
+    "url": "/dadashopping/js/chunk-vendors.e9875a41.js"
+  },
+  {
+    "revision": "dea868d768c8a7d15615f2481361c6a7",
+    "url": "/dadashopping/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dadashopping/robots.txt"
+  }
+]);
